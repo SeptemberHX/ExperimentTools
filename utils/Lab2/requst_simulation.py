e9d312bdc9aed12e14fe814e558d38452f95d534
SeptemberHX <september_hx@outlook.com>
2019-12-06 20:30:25 +0800

    add: request_simulation

--- utils/Lab2/requst_simulation.py
+++ utils/Lab2/requst_simulation.py
@@ -5,21 +5,168 @@
 # @File : requst_simulation.py
 # @Description:
 
-import requests, json, datetime
+import datetime
+import requests
+import json
+import time
+import random
+from utils.common.logger import get_logger
+import threading
 
 
-def do_request(url, data_size_in_kb):
-    start_time = datetime.datetime.now().timestamp() * 1000  # mills
-    r = requests.post(url, json={
-        'valueMap': {
-            'interval': 0
+service_info = {}
+logger = get_logger('request_simulation')
+
+
+def load_service_info(service_json_path):
+    with open(service_json_path) as f:
+        service_data = json.load(f)
+        for service in service_data:
+            if service['serviceName'] not in service_info:
+                service_info[service['serviceName']] = []
+            service_info[service['serviceName']].append(service)
+
+
+def get_response_and_indata_size_with_service_and_function(service_name, function_id):
+    if service_name in service_info:
+        interface_map = service_info[service_name][0]['interfaceMap']
+        for interface in interface_map.values():
+            if interface['functionId'] == function_id:
+                return interface['patternUrl'], interface['inDataSize']
+    return None
+
+
+def do_request_chain(url, user_demand_list):
+    i = 0
+    total_response_time = 0
+    logger.info('======')
+    while i < len(user_demand_list):
+        user_demand = user_demand_list[i]
+        service_name = user_demand['serviceId']
+        function_id = user_demand['functionId']
+        _, in_data_size = get_response_and_indata_size_with_service_and_function(service_name, function_id)
+        msg, response_time = do_request(url, in_data_size, user_demand)
+        if msg == '/fail':
+            logger.info('Failed')
+        else:
+            for j in range(i, len(user_demand_list)):
+                next_service_name = user_demand_list[j]['serviceId']
+                next_function_id = user_demand_list[j]['functionId']
+                next_res_msg, _ = get_response_and_indata_size_with_service_and_function(next_service_name, next_function_id)
+                if next_res_msg == msg:
+                    i = j
+                    break
+            total_response_time += response_time
+        i += 1
+        time.sleep(random.randint(1, 5))
+    logger.info(total_response_time)
+    logger.info('++++++')
+
+
+def do_request(url, data_size_in_kb, user_demand):
+    json_data = {
+        "userDemand": user_demand,
+        "data": {
+            "valueMap": {
+                "interval": 0,
+                "data": '+'*1024*data_size_in_kb
+            }
         }
-    })
+    }
+    start_time = datetime.datetime.now().timestamp() * 1000  # mills
+    r = requests.post(url, json=json_data)
     end_time = datetime.datetime.now().timestamp() * 1000
     interval = r.json()['valueMap']['interval']
+    msg = r.json()['valueMap']['msg']
     response_time = end_time - start_time - interval
-    print(response_time)
+    return msg, response_time
+
+
+def register_user(url, user_info):
+    requests.post(url, json=user_info)
+
+
+def convert_demandchain_to_list(demand_chain):
+    result = []
+    for demand_id in demand_chain['demandIdList']:
+        result.append(demand_chain['demandMap'][demand_id])
+    return result
+
+
+def do_request_user(url, user_info):
+    demand_list_list = []
+    for demand_chain in user_info['demandChainList']:
+        demand_list = convert_demandchain_to_list(demand_chain)
+        demand_list_list.append(demand_list)
+    for demand_list in demand_list_list:
+        do_request_chain(url, demand_list)
+
+
+def simulation_single_user(register_url, gateway_url, user_info_map: dict):
+    """
+    The user_info_map is passed time to user bean.
+    {0: info1, 100: info2, ...} means info1 is the first info. After 100 seconds, changing to info2, etc.
+    """
+    time_list = sorted(user_info_map.keys())
+    assert time_list[0] == 0
+    i = 0
+    register_user(register_url, user_info_map[0])
+    start_timestamp = datetime.datetime.now().timestamp()  # seconds
+    while i < len(time_list):
+        do_request_user(gateway_url, user_info_map[time_list[i]])
+        time.sleep(random.randint(0, 10))
+        curr_timestamp = datetime.datetime.now().timestamp()
+        if i + 1 < len(time_list) and curr_timestamp - start_timestamp > time_list[i + 1]:
+            i += 1
+            register_user(register_url, user_info_map[time_list[i]])
 
 
 if __name__ == '__main__':
-    do_request('http://144.34.200.189:8080/pay', 10)
+    # user_demand = {
+    #     "id": "user_1_market_function"
+    # }
+    # do_request('http://144.34.200.189:30080/request', 200, user_demand)
+    user_demand = {
+        "id": "user_1_market_function",
+        "serviceId": "tencent-service",
+        "functionId": "game_function"
+    }
+    user_demand_2 = {
+        "id": "user_1_pay_function",
+        "serviceId": "bilibili-service",
+        "functionId": "stream_function"
+    }
+    demand_chain = {
+        "demandIdList": [
+            user_demand['id'],
+            user_demand_2['id']
+        ],
+        "demandMap": {
+            user_demand['id']: user_demand,
+            user_demand_2['id']: user_demand_2
+        }
+    }
+    user_info = {
+        "demandChainList": [demand_chain],
+        "position": {
+            "x": 1,
+            "y": 1
+        },
+        "id": 'user_1'
+    }
+    user_info_map = {
+        0: user_info
+    }
+    load_service_info('./service.json')
+    # print(get_response_and_indata_size_with_service_and_function('netease-service', 'game_function'))
+    # do_request_chain('http://144.34.200.189:30080/request', [user_demand])
+    # simulation_single_user(
+    #     register_url='http://144.34.200.189:30080/register',
+    #     gateway_url='http://144.34.200.189:30080/request',
+    #     user_info_map=user_info_map
+    # )
+    threading.Thread(target=simulation_single_user, kwargs={
+        'register_url': 'http://144.34.200.189:30080/register',
+        'gateway_url': 'http://144.34.200.189:30080/request',
+        'user_info_map': user_info_map
+    }).start()
---
+++ utils/common/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# @Time : 2019/12/6 17:32
+# @Author : SeptemberHX
+# @File : __init__.py.py
+# @Description: 
