a04b64110eb5ff9fd509f269fe6c49e1921e028e
SeptemberHX <september_hx@outlook.com>
2019-12-09 10:39:38 +0800

    fix

--- main.py
+++ main.py
@@ -54,17 +54,16 @@ def load_data():
 def start_simulation():
     global target_user_info
     global gateway_ip_port
-    for time in target_user_info:
-        threading.Thread(target=simulation_single_user, kwargs={
-            'register_url': 'http://{0}/register'.format(gateway_ip_port),
-            'gateway_url': 'http://{0}/request'.format(gateway_ip_port),
-            'user_info_map': target_user_info[time]
-        }).start()
+    threading.Thread(target=simulation_single_user, kwargs={
+        'register_url': 'http://{0}/register'.format(gateway_ip_port),
+        'gateway_url': 'http://{0}/request'.format(gateway_ip_port),
+        'user_info_map': target_user_info
+    }).start()
     return ''
 
 
 if __name__ == '__main__':
-    # user_info = load_user_info_from_dict('./lab_data')
     app.run(host='0.0.0.0', port=54321)
+    # user_info = load_user_info_from_dict('./lab_data')
     # target_user_info = fetch_target_user_list(user_info, 0, 300)
     # print(target_user_info[0])
